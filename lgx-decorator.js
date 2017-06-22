2f4efb1025d52ae4cfadfbfd36864b622c05ece1
mc11a7ba <pawel.franczyk@itdirection.pl>
2017-06-22 13:04:45 +0200

    new methods

--- lgx-decorator.js
+++ lgx-decorator.js
@@ -1,67 +1,109 @@
 'use strict';
 
-let decorator = (() => {
+window.Decorator = (() => {
     'use strict';
 
-    let decorate = (className,args) => {
-        if(typeof className === 'string'){
+    let decorate = function (decoratorClass, className, args) {
 
-        }else if(typeof className === 'object' && !Array.isArray(className)){
+        let afterDecore = void 0;
+        let $this = this;
 
-        }else {
+        if (useMethod.length === 0) {
+            throw Error('Not find method for use decorate');
+        } else if (typeof className === 'string') {
+
+            afterDecore = decoratorClass._methodDecorateFn[useMethod][className].apply(this, args);
+
+        } else if (typeof className === 'object' && !Array.isArray(className)) {
+
+        } else {
             throw Error('Invalid decorator name!');
         }
-     };
+
+        useMethod = '';
+
+        return afterDecore;
+
+
+    };
+
+    let useMethod = '';
 
     class Decorator {
-        _baseClass = void 0;
+
         constructor(baseClass) {
-            if (!baseClass || baseClass instanceof Object) {
+            let $this = this;
+            this._baseClass = void 0;
+            this._methodDecorate = {};
+            this._methodDecorateFn = {};
+
+            if (!baseClass || !(baseClass instanceof Object)) {
                 throw Error('Class required!');
             } else {
                 this._baseClass = baseClass;
+                this._baseClass.prototype.decorate = function () {
+
+                    return decorate.apply(this, [$this, ...arguments]);
+
+                }
             }
         }
-        decoration({ decorMethod, fnDecore, name }) {
-            if (this._baseClass.prototype[decorMethod]) {
+
+        _addNewFnDocorate({decorMethod, fnDecor, name}) {
+            let $this = this;
+            if ($this._methodDecorateFn[decorMethod][name]) {
+                throw Error('Decorator exist!');
+            } else {
+                $this._methodDecorateFn[decorMethod][name] = fnDecor;
+            }
+        }
+
+        _createFnDecorator({decorMethod, fnDecor, name}) {
+            let $this = this;
+            $this._methodDecorateFn[decorMethod] = {};
+            $this._methodDecorateFn[decorMethod][name] = fnDecor;
+        }
+
+        decoration({decorMethod, fnDecor, name}) {
+            let $this = this;
+            if ($this._baseClass.prototype[decorMethod]) {
+                if (Object.keys($this._methodDecorate).includes(decorMethod)) {
+                    $this._addNewFnDocorate({decorMethod, fnDecor, name});
+                } else {
+
+                    $this._methodDecorate[decorMethod] = $this._baseClass.prototype[decorMethod];
+                    $this._baseClass.prototype[decorMethod] = function () {
+                        useMethod = decorMethod;
+                        return $this._methodDecorate[decorMethod].apply(this, arguments);
+
+                    };
+
+                    $this._createFnDecorator({decorMethod, fnDecor, name})
+
+                }
+
+            } else if ($this._baseClass[decorMethod]) {
+                if (Object.keys($this._methodDecorate).includes(decorMethod)) {
+                    $this._addNewFnDocorate({decorMethod, fnDecor, name});
+                } else {
+
+                    $this._methodDecorate[decorMethod] = $this._baseClass[decorMethod];
+                    $this._baseClass[decorMethod] = function () {
+                        useMethod = decorMethod;
+                        return $this._methodDecorate[decorMethod].apply(this, arguments);
+
+                    };
+                    $this._createFnDecorator({decorMethod, fnDecor, name})
+
+                }
 
             } else {
-                throw Error('Decration method not exist in this Object');
+                throw Error('Decoration method not exist in this Object');
             }
         }
-    };
+    }
 
     return Decorator;
 
 })();
 
-export {
-    decorator
-};
-
-let decoratorString = new decorator(String);
-decoratorString.decoration({
-    decorMethod: 'small',
-    name: 'class',
-    fnDecore: (self, args) => {
-
-    }
-});
-
-/**
- * @example
-  var text = 'Test TEXT';
-  text
-    .small()
-    .decorate('class', ['small__color--red'])
-    .decorate('transparent-hover', [0.67]);
-
-  text
-    .small()  
-    .decorate({
-        'class': ['small__color--red'],
-        'transparent-hover': [0.67]
-    });
-
-  text === '<small class="small__color--red">Test TEXT</small>';
- */
